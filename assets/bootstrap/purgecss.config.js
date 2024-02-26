9b73848111d33cfc45317056c6918b0f5f03db61
Loïc Tallec <loictallec1@gmail.com>
2024-02-26 10:36:25 +0100

    trim img + tinypng

--- assets/bootstrap/purgecss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    content: ['../../index.html', 'bootstrap.min.css'], // Spécifiez les fichiers dans lesquels PurgeCSS recherchera des classes non utilisées
-    css: ['bootstrap.min.css'], // Spécifiez les fichiers CSS que vous souhaitez purger
-    // Autres options de configuration
-    // ...
-}
